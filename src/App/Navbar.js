48fd12e527c9cc61ecc71199d18207da23cadb21
Quinn <74791898+griffique@users.noreply.github.com>
2023-08-27 13:46:35 -0400

    update blog link

--- src/App/Navbar.js
+++ src/App/Navbar.js
@@ -1,25 +1,30 @@
-import './App.css';
-
+import "./App.css";
 
 function Navbar() {
-    return (
-      <nav className="navbar is-black" role="navigation">
-        <div id="navbarItems">
-          <div className="navbar-start">
-            <a className="navbar-item nav-link" href="/">
-              <img alt="OM logo" src="/favicon-32x32.png"></img>
-            </a>
-            <a className="navbar-item nav-link" href="#meditate">
-              Meditate</a>
-            <a className="navbar-item nav-link" href="https://quinneringriffin.com" target="_blank" rel="noreferrer">
-              Blog</a>
-            <a className="navbar-item nav-link" href="#faqs">
-              FAQs
-            </a>
-          </div>
+  return (
+    <nav className="navbar is-black" role="navigation">
+      <div id="navbarItems">
+        <div className="navbar-start">
+          <a className="navbar-item nav-link" href="/">
+            <img alt="OM logo" src="/favicon-32x32.png"></img>
+          </a>
+          <a className="navbar-item nav-link" href="#meditate">
+            Meditate
+          </a>
+          <a
+            className="navbar-item nav-link"
+            href="https://IncidentalBuddhist.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Blog
+          </a>
+          <a className="navbar-item nav-link" href="#faqs">
+            FAQs
+          </a>
         </div>
-      </nav>
-    );
-  }
-  export default Navbar;
-  
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
